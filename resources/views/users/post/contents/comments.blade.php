db0db5f791f201ee59dc58f2e8c8c1a85ee9a887
kurt john <kredoteacher.kurt@gmail.com>
2024-08-16 15:21:08 +0800

    added comments

--- resources/views/users/post/contents/comments.blade.php
+++ resources/views/users/post/contents/comments.blade.php
@@ -2,13 +2,13 @@
     {{-- Show all comments here --}}
     @if ($post->comments->isNotEmpty())
         <ul class="list-group mt-2">
-            @foreach ($post->comments as $comment)
+            @foreach ($post->comments->take(3) as $comment)
                 <li class="list-group-item border-0 p-0 mb-2">
                     <a href="#" class="text-decoration-none text-dark fw-bold">{{ $comment->user->name }}</a>
                     &nbsp;
                     <p class="d-inline fw-light">{{ $comment->body }}</p>
 
-                    <form action="{{route('comment.destroy',$comment)}}" method="post">
+                    <form action="{{ route('comment.destroy', $comment) }}" method="post">
                         @csrf
                         @method('DELETE')
 
@@ -25,6 +25,11 @@
                     </form>
                 </li>
             @endforeach
+            @if ($post->comments->count() > 3)
+                <li class="list-group-item border-0 p-0 mb-2">
+                    <a href="{{route('post.show',$post)}}" class="text-decoration-none">View all {{ $post->comments->count() }} comments</a>
+                </li>
+            @endif
         </ul>
     @endif
 
@@ -32,9 +37,8 @@
         @csrf
 
         <div class="input-group">
-            <input type="hidden" name="post_id" value="{{$post->id}}">
-            <textarea name="body" rows="1"
-                class="form-control form-control-sm" placeholder="Add a comment...">{{ old('comment_body' . $post->id) }}</textarea>
+            <input type="hidden" name="post_id" value="{{ $post->id }}">
+            <textarea name="body" rows="1" class="form-control form-control-sm" placeholder="Add a comment...">{{ old('comment_body' . $post->id) }}</textarea>
             <button class="btn btn-secondary btn-sm">Post</button>
         </div>
         {{-- Error --}}
